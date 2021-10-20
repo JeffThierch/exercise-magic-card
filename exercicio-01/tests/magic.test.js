69d8e6c441ee870ccad43244321f92fd629d5786
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-20 11:41:11 -0300

    Exercicio 03 - Completo

--- exercicio-01/tests/magic.test.js
+++ exercicio-01/tests/magic.test.js
@@ -11,13 +11,14 @@ const expected = {
 
 describe(' Testa a função getMagicCard', () => {
   it('Deve retornar um objeto com as propriedades esperadas', async () => {
-    expect.assertions(2);
+    expect.assertions(3);
     const getMagicCardSimulator = trybeSimulator(magic, 'getMagicCard');
     const usedTest = await getMagicCardSimulator('130550');
 
     // implemente seus testes aqui    
    expect(usedTest).toMatchObject(expected)
    expect(Array.isArray(usedTest.types)).toBeTruthy()
+   expect(usedTest.subtypes.length).toBe(2)
   });
 });
 
