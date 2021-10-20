4dfb6c8bad7d328e4dba616d5af17ee8aec473f3
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-20 12:38:37 -0300

    Exercicio 2 / 01 e 02 git push

---
+++ exercicio-02/tests/magic.test.js
@@ -0,0 +1,28 @@
+const { getMagicCard } = require('../src/magic.js');
+const favoriteCards = require('../data/favoriteCards.js');
+
+const retrievesFavoriteCards = () => {
+  // implemente sua função aqui
+}
+
+describe(' Testa a função getMagicCard', () => { 
+
+  it('Testa se um novo card é adicionado a cada execução', async () => {
+    expect.assertions(2);
+    expect.assertions();
+    await getMagicCard('130553');
+
+    // implemente seus testes aqui
+    expect(favoriteCards.length).toBe(5);
+    expect(favoriteCards[favoriteCards.length - 1].name).toMatch('Beacon of Immortality')
+
+  });
+
+  it('Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
+    // expect.assertions();
+
+    // expect(favoriteCards).toHaveLength(4);
+    // implemente seus testes aqui
+
+  });
+});
