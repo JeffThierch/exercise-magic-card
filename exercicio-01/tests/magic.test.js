c4c7f29bb14c00ff84e3f16d6ae8cdc1edaf1e38
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-10-20 11:34:16 -0300

    Exercicio 01 - Completo

---
+++ exercicio-01/tests/magic.test.js
@@ -0,0 +1,21 @@
+const magic = require('../src/magic.js');
+const { trybeSimulator } = require('../simulator/simulator.js');
+
+const expected = {
+  name: "Ancestor's Chosen",
+  manaCost: '{5}{W}{W}',
+  types: [ 'Creature' ],
+  subtypes: [ 'Human', 'Cleric' ],
+  rarity: 'Uncommon'
+};
+
+describe(' Testa a função getMagicCard', () => {
+  it('Deve retornar um objeto com as propriedades esperadas', async () => {
+    expect.assertions(1);
+    const getMagicCardSimulator = trybeSimulator(magic, 'getMagicCard');
+    await getMagicCardSimulator('130550');
+
+    // implemente seus testes aqui    
+   expect(await getMagicCardSimulator('130550')).toMatchObject(expected)
+  });
+});
